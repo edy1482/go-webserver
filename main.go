f376b0d04e8275dc0167018d297a09ba5f732bf1
edy1482 <edward@ritchotte.com>
2025-09-07 15:10:23 -0500

    hello world from http server

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	msg := "Hello World"
-	fmt.Println(msg)
+	// Register a handler function for the root path "/"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Go HTTP Server!")
+	})
+
+	// Start the server on port 8080
+	fmt.Println("Server listening on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
